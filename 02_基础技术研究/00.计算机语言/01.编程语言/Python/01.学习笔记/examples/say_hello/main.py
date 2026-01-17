d268ea6e2bb26c96928a760adce8f1c33c66c167
owlman <lingjiexyz@hotmail.com>
2026-01-17 14:20:20 +0800

    整理Python学习笔记：基础语法学习

--- 02_基础技术研究/00.计算机语言/01.编程语言/Python/01.学习笔记/examples/say_hello/main.py
+++ 02_基础技术研究/00.计算机语言/01.编程语言/Python/01.学习笔记/examples/say_hello/main.py
@@ -6,6 +6,7 @@
     Created on: 2024-06-15
     '''
 
+author_name = "owlman"  # global variable
 def say_hello(name: str) -> None:
     '''
         Say hello by name.
@@ -14,7 +15,17 @@ def say_hello(name: str) -> None:
             name: name to greet
     '''
     print("Hello World! My name is", name)
+    message = "\nThis is an object-oriented,open-source programming language often used for rapid application development.Python's simple syntax amphasizes readability,reducing the cost of program mantenance, while its large library of functions and calls encourages reuse and extensibility."
+    print(message)
+    print("") # print a blank line
+
+def say_hello_to_me() -> None:
+    '''
+        Say hello to myself.
+    '''
+    author_name = "lingjie" # local variable
+    say_hello(author_name)
 
 if __name__ == "__main__":
-    author_name = "lingjie"    
-    say_hello(author_name)
+    say_hello(author_name) 
+    say_hello_to_me()      
